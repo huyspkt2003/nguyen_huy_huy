c743b45d84027dcf3cdd8037d6aef6b0ab74fc8f
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-06 19:50:31 +0700

    tim so lon nhat cua a va b

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+
+
 int main()
 {
-	printf ("xin chao\n");
+	int x = 0;
+	int y = 0;
+	printf("hay nhap so nguyen x\n");
+	scanf_s ( "%d", &x);
+	printf("hay nhap so nguyen y\n");
+	scanf_s("%d", &y);
+	if (x >= y)
+	{
+		printf("so lon nhat la:%d",x);
+	}
+	else
+	{
+		printf("so lon nhat la:%d", y);
+	}
 }
 
 
