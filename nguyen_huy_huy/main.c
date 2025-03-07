c95707294c6101b228b391aec8b2150850d0134a
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-07 19:57:22 +0700

    Bai 4: Kiem tra am duong

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -4,36 +4,41 @@
 int main()
 {
 	int a = 0;
-	int b = 0;
-	int c = 0;
-	int delta = 0;
-	int no = 0;
-	int no_kep = 0;
-	int x1 = 0;
-	int x2 = 0;
-	printf(" Hay nhap vao so a:");
-	scanf_s("%d",&a);
-	printf(" Hay nhap vao so b:");
-	scanf_s("%d", &b);
-	printf(" Hay nhap vao so c:");
-	scanf_s("%d", &c);
-	delta = b*b - (4 * a * c);
+	int n = 0;
+	while (n != 2)
+	{
 
-	if ( delta<0 )
-	{
-		printf("pt vo nghiem");
+		switch (n)
+		{
+		case 0:
+			printf("Hay nhap so:");
+			scanf_s("%d", &a);
+			if (a == 0)
+			{
+				n = 0;
+			}
+			else
+			{
+				n = 1;
+			}
+			break;
+		case 1:
+			if (a > 0)
+			{
+				printf("Day la so duong");
+				n = 2;
+
+			}
+			else
+			{
+				printf("Day la so am");
+				n = 2;
+			}
+			break;
+		}
 	}
-	else if (delta == 0)
-	{
-		no_kep = -b / (2 * a);
-		printf("pt co nghiem kep la x1=x2=:%d", no_kep );
-	}
-	else 
-	{
-		x1 = (-b + sqrt(delta)) / (2 * a);
-		x2 = (-b - sqrt(delta)) / (2 * a);
-		printf("phuong trinh co 2 nghiem la x1 =:%d\n x2=%d" , x1, x2 );
-		
-	}
+
+
+
 
 }
