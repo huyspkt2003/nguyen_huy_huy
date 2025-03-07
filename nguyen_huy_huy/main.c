01c432b65d78a08b72f62d6f6731039a2eaa03eb
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-07 19:46:06 +0700

    Bai 3: Tim nghiem cua pt

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
-
+#include <math.h>
 
 int main()
 {
 	int a = 0;
 	int b = 0;
 	int c = 0;
+	int delta = 0;
+	int no = 0;
+	int no_kep = 0;
+	int x1 = 0;
+	int x2 = 0;
 	printf(" Hay nhap vao so a:");
 	scanf_s("%d",&a);
 	printf(" Hay nhap vao so b:");
 	scanf_s("%d", &b);
 	printf(" Hay nhap vao so c:");
 	scanf_s("%d", &c);
-	if (a > b && a > c)
+	delta = b*b - (4 * a * c);
+
+	if ( delta<0 )
 	{
-		printf("So lon nhat la:%d", a);
+		printf("pt vo nghiem");
 	}
-	else if (b > a && b > c)
+	else if (delta == 0)
 	{
-		printf("So lon nhat la:%d", b );
+		no_kep = -b / (2 * a);
+		printf("pt co nghiem kep la x1=x2=:%d", no_kep );
 	}
 	else 
 	{
-		printf("So lon nhat la:%d", c );
+		x1 = (-b + sqrt(delta)) / (2 * a);
+		x2 = (-b - sqrt(delta)) / (2 * a);
+		printf("phuong trinh co 2 nghiem la x1 =:%d\n x2=%d" , x1, x2 );
+		
 	}
 
 }
