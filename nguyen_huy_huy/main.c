0837d97ff3945c7c7d1ae03282cf1bbdb3e9eea3
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-11 14:47:51 +0700

    Bai 7 : Tinh tien dien

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,42 +3,39 @@
 
 int main()
 {
-	int a = 0;
+	float so = 0;
 	int n = 0;
-	while (n != 2)
+	float tien = 2;
+
+	printf("Hay nhap so:");
+	scanf_s("%f", &so);
+	if (0 < so && so < 50)//bac1
 	{
-
-		switch (n)
-		{
-		case 0:
-			printf("Hay nhap so:");
-			scanf_s("%d", &a);
-			if (a == 0)
-			{
-				n = 0;
-			}
-			else
-			{
-				n = 1;
-			}
-			break;
-		case 1:
-			if ((a+2)%2 == 0)
-			{
-				printf("Day la so chan");
-				n = 2;
-
-			}
-			else
-			{
-				printf("Day la so le");
-				n = 2;
-			}
-			break;
-		}
+		tien = so * 1.678;
 	}
-
-
-
-
+	else if (so < 100)//bac2
+	{
+		tien = 50 * 1.678 + (so - 50) * 1.734;
+	}
+	else if (so < 200)//bac3
+	{
+		tien = 50 * 1.678 + 50 * 1.734 + (so - 100) * 2.014;
+	}
+	else if (so < 300)//bac4
+	{
+		tien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + (so - 200) * 2.536;
+	}
+	else if (so < 400)//bac5
+	{
+		tien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + (so - 300) * 2.834;
+	}
+	else//bac6
+	{
+		tien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + 100 * 2.834 + (so -400)*2.927;
+	}
+	printf("Tong so tien phai tra: %f", tien);
 }
+
+
+
+
