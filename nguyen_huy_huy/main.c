135802c8925668c883facc045e70e8e1822c8e68
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-11 19:50:07 +0700

    Bang cuu chuong 2

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,37 +3,13 @@
 
 int main()
 {
-	float so = 0;
-	int n = 0;
-	float tien = 2;
-
-	printf("Hay nhap so:");
-	scanf_s("%f", &so);
-	if (0 < so && so < 50)//bac1
+	int kq = 0;
+	for (int i = 0; i < 11; i++)
 	{
-		tien = so * 1.678;
+		kq = 2 * i;
+		printf("2 x %d = %d \n",i, kq);
+		
 	}
-	else if (so < 100)//bac2
-	{
-		tien = 50 * 1.678 + (so - 50) * 1.734;
-	}
-	else if (so < 200)//bac3
-	{
-		tien = 50 * 1.678 + 50 * 1.734 + (so - 100) * 2.014;
-	}
-	else if (so < 300)//bac4
-	{
-		tien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + (so - 200) * 2.536;
-	}
-	else if (so < 400)//bac5
-	{
-		tien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + (so - 300) * 2.834;
-	}
-	else//bac6
-	{
-		tien = 50 * 1.678 + 50 * 1.734 + 100 * 2.014 + 100 * 2.536 + 100 * 2.834 + (so -400)*2.927;
-	}
-	printf("Tong so tien phai tra: %f", tien);
 }
 
 
