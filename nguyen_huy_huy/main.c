0ee4c763a16c773ddfaac84287a807e50de8cefe
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-12 18:04:33 +0700

    Bai 4: Tinh lai kep ngan hang

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,16 +3,20 @@
 
 int main()
 {
+	float tien_gui = 0;
+	float tien_nhan = 0;
+	int i = 0;
 	int n = 0;
-	int sl = 0;
-	do
+	printf("nhap tien gui: ");
+	scanf_s("%f", &tien_gui);
+	printf("nhap so nam gui: ");
+	scanf_s("%d", &n);
+	tien_nhan = tien_gui;
+	for (int i = 0 ;i < n ; i++)
 	{
-		printf("nhap so nguyen: ");
-		scanf_s("%d", &n);
-		if (n ==
-			0)
-			sl++;
-	} while (sl <= 5);
+			tien_nhan *=  (1+ 0.082);
+	}
+	printf("so tien nhan duoc la:%f", tien_nhan);
 }
 
 
