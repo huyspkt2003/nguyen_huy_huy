afadfd625d041311604bac84151a7963636018ad
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-12 21:46:33 +0700

    Bai 5 : In so nguyen to nho hon n

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,20 +3,25 @@
 
 int main()
 {
-	float tien_gui = 0;
-	float tien_nhan = 0;
-	int i = 0;
+	int nhap = 0;
 	int n = 0;
-	printf("nhap tien gui: ");
-	scanf_s("%f", &tien_gui);
-	printf("nhap so nam gui: ");
-	scanf_s("%d", &n);
-	tien_nhan = tien_gui;
-	for (int i = 0 ;i < n ; i++)
-	{
-			tien_nhan *=  (1+ 0.082);
-	}
-	printf("so tien nhan duoc la:%f", tien_nhan);
+	printf("nhap so nguyen: ");
+	scanf_s("%d", &nhap);
+		for (int i = 0; i < nhap; i++)
+		{
+			for (int j = 1; j <= i; j++)
+			{
+				if (i % j == 0)
+				n++;
+					
+			}
+			if (n == 2)
+				printf("%d\t", i);
+			n = 0;
+		}
+
+	
+	
 }
 
 
