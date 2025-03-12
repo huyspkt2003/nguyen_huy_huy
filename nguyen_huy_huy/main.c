0fb8b44d5e332013db8b3eb86ad10e4eb86b503c
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-12 16:11:33 +0700

    Bai 1: cong so tu 0 den n

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,40 +3,15 @@
 
 int main()
 {
+	int n = 0;
 	int tong = 0;
-	int nhap = 0;
-	int n50 = 0;
-	int n100 = 0;
-	int n200 = 0;
-	int n500 = 0;
-	int ch = 0;
-	printf("nhap so tien: ");
-	//scanf_s("%d", &nhap);
-	scanf_s("%d", &nhap);
-	while (nhap != tong)
+	printf("nhap so nguyen: ");
+	scanf_s("%d", &n);
+	for (int np = 0; np <= n; np++)
 	{
-		if (nhap - tong >= 50)
-		{
-			n50++;
-			tong = n50*50+ n100*100+ n200*200+ n500*500;
-		}
-		if (nhap - tong >= 100)
-		{
-			n100++;
-			tong = n50 * 50 + n100 * 100 + n200 * 200 + n500 * 500;
-		}
-		if (nhap - tong >= 200)
-		{
-			n200++;
-			tong = n50 * 50 + n100 * 100 + n200 * 200 + n500 * 500;
-		}
-		if (nhap - tong >= 500)
-		{
-			n500++;
-			tong = n50 * 50 + n100 * 100 + n200 * 200 + n500 * 500;
-		}
+		tong = tong + np;
 	}
-	printf("%d x 50\n %d x 100\n %d x 200\n %d x 500\n ", n50, n100, n200, n500  );
+	printf("%d", tong );
 
 	
 }
