4bfebbc6ba1f6aaddf4fcd8b4af3926819b2c936
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-12 15:48:59 +0700

    Bai 9: Mo phong tra tien cua may ATM

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,18 +3,42 @@
 
 int main()
 {
-	int kq = 0;
-	for (int i = 0; i < 11; i++)
+	int tong = 0;
+	int nhap = 0;
+	int n50 = 0;
+	int n100 = 0;
+	int n200 = 0;
+	int n500 = 0;
+	int ch = 0;
+	printf("nhap so tien: ");
+	//scanf_s("%d", &nhap);
+	scanf_s("%d", &nhap);
+	while (nhap != tong)
 	{
-		for (int j = 2; j < 10; j++)
+		if (nhap - tong >= 50)
 		{
-			kq = j * i;
-			printf("%d x %d = %d \t",j, i, kq);
-			
-
+			n50++;
+			tong = n50*50+ n100*100+ n200*200+ n500*500;
 		}
-		printf("\n");
+		if (nhap - tong >= 100)
+		{
+			n100++;
+			tong = n50 * 50 + n100 * 100 + n200 * 200 + n500 * 500;
+		}
+		if (nhap - tong >= 200)
+		{
+			n200++;
+			tong = n50 * 50 + n100 * 100 + n200 * 200 + n500 * 500;
+		}
+		if (nhap - tong >= 500)
+		{
+			n500++;
+			tong = n50 * 50 + n100 * 100 + n200 * 200 + n500 * 500;
+		}
 	}
+	printf("%d x 50\n %d x 100\n %d x 200\n %d x 500\n ", n50, n100, n200, n500  );
+
+	
 }
 
 
