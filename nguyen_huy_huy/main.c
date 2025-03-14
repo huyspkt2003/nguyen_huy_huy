474f7965d8fb064d28f7f4dd7f3d333b93f52914
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-14 17:57:05 +0700

    Bài 5: Xây dụng hàm. in ra các giá trị LẼ trong mảng arr

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,16 +3,16 @@
 
 int main()
 {
-	int arr[] = { -1,2,-3,4,-7 };
+	int arr[] = { 1,2,3,4,7 };
 	int num_elements = (sizeof(arr) / sizeof(arr[0])) - 1;
 	float sum_odd = 0;
 	float odd = 0;
 
 	for (int i = 0; i <= num_elements; i++)
 	{
-		if (arr[i] < 0)
+		if (arr[i] %2 !=0)
 		{
-			arr[i] = 0;
+			printf("%d\t", arr[i]);
 		}
 	}
 	
