2982fe85a9998694d449be6743f31c6b65f0ae64
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-14 17:49:25 +0700

    Bài 1: Xây dựng hàm. tính tổng các giá trị trong mảng arr

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,20 +3,17 @@
 
 int main()
 {
-	float a = 0;
-	float b = 0;
-	float h = 0;
-	float kq = 0;
-	printf("Nhap a: ");
-	scanf_s("%f", &a);
-	printf("Nhap b: ");
-	scanf_s("%f", &b);
-	h = (b-a) / 1000;
-	for (int i = 0; i < 999; i++)
+	int arr[] = { 1,2,3 };
+	int num_elements = (sizeof(arr) / sizeof(arr[0])) - 1;
+	int sum = 0;
+
+	for (int i = 0; i <= num_elements; i++)
 	{
-		kq += (pow((a + i * h), 2) + pow((a + (1 + i) * h), 2))*h/2;
+		sum = sum + arr[i];
 	}
-	printf("Ket qua la:%f", kq);
+	printf("The sum of elements in the array is: %d", sum);
+
+
 }
 
 
