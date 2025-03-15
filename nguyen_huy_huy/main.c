94c0feeac364e53847b1cb5abdd70a60294e21a4
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-15 17:14:24 +0700

    Bài 6: Xây dụng hàm. in ra các giá trị là số nguyên tố trong mảng arr

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,22 +3,27 @@
 
 int main()
 {
-	int arr[] = { 1,2,3,4,7 };
-	int num_elements = (sizeof(arr) / sizeof(arr[0])) - 1;
-	float sum_odd = 0;
-	float odd = 0;
-
-	for (int i = 0; i <= num_elements; i++)
-	{
-		if (arr[i] %2 !=0)
-		{
-			printf("%d\t", arr[i]);
-		}
-	}
+	int arr[] = {1,2,3,4,5,11,22,33,6,7,8,73};
+	int sum_of_ele = sizeof(arr) / sizeof(arr[0]);
+	int uocchung = 0;
 	
+	for (int i = 0; i < sum_of_ele ; i++)
+	{
+		for (int j = 1; j <= arr[i]; j++)
+		{
+			if (arr[i] % j == 0)
+				uocchung++;
+
+		}
+		if (uocchung == 2)
+			printf("%d\t", arr[i]);
+		 uocchung = 0;
+	}
+
 
 
 }
+
 
 
 
