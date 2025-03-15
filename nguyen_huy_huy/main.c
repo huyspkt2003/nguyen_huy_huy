cc84828b8381903a24bd0ae8576ef48326483d01
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-15 17:16:18 +0700

    Bài 7: Xây dựng hàm. in ra giá trị lớn nhất trong mảng arr

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,26 +3,24 @@
 
 int main()
 {
-	int arr[] = {1,2,3,4,5,11,22,33,6,7,8,73};
+	int arr[] = { 1,2,2,4,5,4,6,5,6,100 };
+	int max = 0;
 	int sum_of_ele = sizeof(arr) / sizeof(arr[0]);
-	int uocchung = 0;
-	
-	for (int i = 0; i < sum_of_ele ; i++)
-	{
-		for (int j = 1; j <= arr[i]; j++)
-		{
-			if (arr[i] % j == 0)
-				uocchung++;
 
+	for (int i = 0; i < sum_of_ele; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
 		}
-		if (uocchung == 2)
-			printf("%d\t", arr[i]);
-		 uocchung = 0;
 	}
+	printf("%d\t", max);
+	
 
 
 
 }
+
 
 
 
