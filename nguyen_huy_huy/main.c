9f22c728ee88089f818509b486e77c41f1882e9f
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-15 18:05:44 +0700

    Bài 9: Xây dựng hàm. Sắp xếp mảng arr theo giá trị tăng dần

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -3,21 +3,25 @@
 
 int main()
 {
-	int arr[] = { 1,2,2,4,5,4,6,5,6,100 };
-	int max = 0;
+	int arr[] = { 6,7,5,4,2,8,5,6,4 };
+	int min_temp = 0;
 	int sum_of_ele = sizeof(arr) / sizeof(arr[0]);
-
-	for (int i = 0; i < sum_of_ele; i++)
-	{
-		if (arr[i] > max)
-		{
-			max = arr[i];
-		}
-	}
-	printf("%d\t", max);
+	int temp = 0;
 	
-
-
+	
+		for (int i = 0; i < sum_of_ele-1; i++)
+		{
+			for (int i = 0; i < sum_of_ele - 1; i++)
+			{
+				if (arr[i] > arr[i + 1])
+				{
+					temp = arr[i + 1];
+					arr[i + 1] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+		
 
 }
 
