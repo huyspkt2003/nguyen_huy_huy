5a89fab0192afb0c98bee5e97de55e837c6492c6
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-17 19:24:56 +0700

    Bài 2: Xây dựng hàm. dùng để điếm có bao nhiêu ký tự space (dấu cách) trong chuỗi

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-char str[] = "hello";
-int sum = 0;
+char str[] = "hello world , I'm GPT";
+int sum_space = 0;
 
 int main()
 {
 	int size = sizeof(str) / sizeof(str[0])-1; 
 	for (int i = 1; i <= size; i++)
 	{
-		sum++;
+		if(str[i] == 32)
+			sum_space++;
 	}
-	printf("%d", sum);
+	printf("%d", sum_space);
 
 }
 
