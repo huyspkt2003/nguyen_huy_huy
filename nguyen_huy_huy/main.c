48067d1208b9f0aaf6dd321ced220195cb90570e
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-17 19:42:57 +0700

    Bài 4: Xây dựng hàm. dùng để điếm số từ (word) trong chuỗi

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-char str[] = "Hello world , I'm HuyHuy";
-
+char str[] = "     this is     a    string   ";
+sum_word = 0;
 
 int main()
 {
 	int size = sizeof(str) / sizeof(str[0])-1; 
 	for (int i = 1; i <= size; i++)
 	{
-		if(str[i] >= 97)
-			str[i]= str[i]- 32;
+		if(str[i] == 32 && str[i+1] != 32 && str[i + 1] != 0)
+			sum_word ++;
 	}
-	printf("%s", str);
+	printf("%d", sum_word);
 
 }
 
