f81bf7510e9a9e17c036c9a3f6cef3866bab6109
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-17 19:55:54 +0700

    Bai 6: Xây dựng hàm. dùng để tìm ký tự c trong chuỗi str. hàm trả về địa chỉ của ký tự c trong chuỗi str

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-char str[] = "nguyen van a";
-sum_word = 0;
+char str[] = "con cho con ";
+
 
 int main()
 {
 	int size = sizeof(str) / sizeof(str[0])-1; 
 	for (int i = 0; i <= size; i++)
 	{
-		if (str[i] == 32)
+		if (str[i] == 99)
+		{ 
 			str[i + 1] -= 32;
+			printf("%d\t", i);
+		}
 	}
-	str[0] -= 32;
-	printf("%s", str);
 
 }
 
