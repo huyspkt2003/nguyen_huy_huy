cf17b2f3db6eec4f9f4a3b256b07ca52ae3b1c45
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-17 19:30:41 +0700

    Bài 3: Xây dụng hàm, dùng để chuyễn chữ thường thành chư IN HOA

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-char str[] = "hello world , I'm GPT";
-int sum_space = 0;
+char str[] = "Hello world , I'm HuyHuy";
+
 
 int main()
 {
 	int size = sizeof(str) / sizeof(str[0])-1; 
 	for (int i = 1; i <= size; i++)
 	{
-		if(str[i] == 32)
-			sum_space++;
+		if(str[i] >= 97)
+			str[i]= str[i]- 32;
 	}
-	printf("%d", sum_space);
+	printf("%s", str);
 
 }
 
