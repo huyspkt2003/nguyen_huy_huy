95275eefac675c78646f0244df04668783d7e835
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-17 19:22:23 +0700

    Bài 1: Xây dụng hàm. dùng để điếm số ký tự trong chuỗi

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
+char str[] = "hello";
+int sum = 0;
+
 int main()
 {
-	int arr[] = { 6,7,5,4,2,8,5,6,4 };
-	int min_temp = 0;
-	int sum_of_ele = sizeof(arr) / sizeof(arr[0]);
-	int temp = 0;
-	
-	
-		for (int i = 0; i < sum_of_ele-1; i++)
-		{
-			for (int i = 0; i < sum_of_ele - 1; i++)
-			{
-				if (arr[i] > arr[i + 1])
-				{
-					temp = arr[i + 1];
-					arr[i + 1] = arr[i];
-					arr[i] = temp;
-				}
-			}
-		}
-		
+	int size = sizeof(str) / sizeof(str[0])-1; 
+	for (int i = 1; i <= size; i++)
+	{
+		sum++;
+	}
+	printf("%d", sum);
 
 }
 
