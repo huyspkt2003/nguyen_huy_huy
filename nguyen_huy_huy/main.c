0227fa91d9177ef259f25715bd8939c8f02c5c9a
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-17 19:51:27 +0700

    Bài 5: Xây dụng hàm. dùng để in hoa chữ cái đầu tiên của từ trong chuỗi str

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-char str[] = "     this is     a    string   ";
+char str[] = "nguyen van a";
 sum_word = 0;
 
 int main()
 {
 	int size = sizeof(str) / sizeof(str[0])-1; 
-	for (int i = 1; i <= size; i++)
+	for (int i = 0; i <= size; i++)
 	{
-		if(str[i] == 32 && str[i+1] != 32 && str[i + 1] != 0)
-			sum_word ++;
+		if (str[i] == 32)
+			str[i + 1] -= 32;
 	}
-	printf("%d", sum_word);
+	str[0] -= 32;
+	printf("%s", str);
 
 }
 
