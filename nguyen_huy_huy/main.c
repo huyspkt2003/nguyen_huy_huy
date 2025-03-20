094b537bfa03e344c43a7954666f92f99cfb0276
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-20 16:23:24 +0700

    Bài 3: Xây dụng hàm, dùng để chuyễn chữ thường thành chư IN HOA

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,33 +1,31 @@
 ﻿#include <stdio.h>
 #include <math.h>
 
-//Chức năng: Xây dựng hàm. dùng để điếm có bao nhiêu ký tự space (dấu cách) trong chuỗi
+//Chức năng: dùng để chuyễn chữ thường thành chư IN HOA
 //Input: chuỗi
 // (char* string)
-//Output: so ki tu space
-int sum_space(char* string)
+//Output: no
+void upper_case(char* string)
 {
 	int sum_ele = 0;
 	int sum_space = 0;
 	while (string [sum_ele] != 0)
 	{
-		if (string[sum_ele] == ' ')
+		if (string[sum_ele] >= 97)
 		{
-			sum_space++;
+			string[sum_ele] -= 32;
 		}
 		sum_ele++;
 
 	}
-	return sum_space;
-	
 }
 
 int main()
 {
 	int tong = 0;
-	char str[] = "hello world hihi";
-	tong = sum_space(str);
-	printf("%d", tong);
+	char str[] = "Hello world hihi";
+	upper_case(str);
+	
 
 }
 
