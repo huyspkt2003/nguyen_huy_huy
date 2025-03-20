76de4feed7a496f1cd0c4cc49f37e8535f2e545b
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-20 16:53:22 +0700

    Bài 5: Xây dụng hàm. dùng để in hoa chữ cái đầu tiên của từ trong chuỗi str "nguyen van a" -> "Nguyen Van A"

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,19 +1,21 @@
 ﻿#include <stdio.h>
 #include <math.h>
 
-//Chức năng: dùng để chuyễn chữ thường thành chư IN HOA
+//Chức năng: dùng để in hoa chữ cái đầu tiên của từ trong chuỗi str
+//"nguyen van a" -> "Nguyen Van A"
 //Input: chuỗi
 // (char* string)
-//Output: so tu trong chuoi
-void sum_word(char* string)
+//Output: no
+void upper_word(char* string)
 {
 	int sum_ele = 0;
 	int sum_word = 0;
+	string[0] -= 32;
 	while (string [sum_ele] != 0)
 	{
 		if (string[sum_ele] == 32 && string[sum_ele+1]!=32  )
 		{
-			sum_word++;
+			string[sum_ele + 1] -= 32;
 		}
 		sum_ele++;
 
@@ -24,8 +26,8 @@ void sum_word(char* string)
 int main()
 {
 	int tong = 0;
-	char str[] = " Hello world hihi";
-	sum_word(str);
+	char str[] = "nguyen huu huy";
+	upper_word(str);
 	
 
 }
