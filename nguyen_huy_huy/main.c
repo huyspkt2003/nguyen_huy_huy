a962cbdfbb6c040cc36fad28f0851d3013859b04
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-20 09:42:47 +0700

    Bài 1:Xây dựng hàm. tính tổng các giá trị trong mảng arr

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,21 +1,28 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <math.h>
 
-char str[] = "con cho con ";
+//Chức năng: Tính tổng giá trị của mảng
+//Input: mảng, số lượng phần tử
+// (int* array, so_luong)
+//Output: Tổng của các phần tử trong mảng
+int sum_array(int* array, int num_elements)
+{
+	int sum = 0;
+	for (int i = 0; i < num_elements; i++)
+	{
+		sum = sum + *(array+i);
+	}
+	return sum;
+
+}
 
 
 int main()
 {
-	int size = sizeof(str) / sizeof(str[0])-1; 
-	for (int i = 0; i <= size; i++)
-	{
-		if (str[i] == 99)
-		{ 
-			str[i + 1] -= 32;
-			printf("%d\t", i);
-		}
-	}
-
+	int tong = 0;
+	int arr[] = { 1,2,3 };
+	tong = sum_array(arr, 3);
+	printf("The sum of element of array is:%d", tong);
 }
 
 
