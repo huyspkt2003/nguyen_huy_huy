92d7c5bb37e753a8ec3c2ba3f9874a9bffe36de1
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-20 11:06:26 +0700

    Bài 6: Xây dụng hàm. in ra các giá trị là số nguyên tố trong mảng arr

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,29 +1,38 @@
 ﻿#include <stdio.h>
 #include <math.h>
 
-//Chức năng: in ra các giá trị LẼ trong mảng arr
+//Chức năng: in ra các giá trị la số nguyên tố trong array
 //Input: mảng, số lượng phần tử
 // (int* array, so_luong)
 //Output: no
 void position(int* arr, int num_elements)
 {
-	
-	int max = 0;
+	int uocchung;
 
 	for (int i = 0; i < num_elements; i++)
 	{
-		if (*(arr + i)%2 !=0)
+		uocchung = 0;
+		
+			for (int j = 1; j <= *(arr + i); j++)
+			{
+			
+					if (*(arr + i) % j == 0)
+							   uocchung++;
+
+			}
+		if (uocchung == 2)
 		{
-			 printf("%d\t", * (arr + i));
+			printf(" % d\t", *(arr + i));
+			
 		}
+			
 	}
 	
 }
 
-
 int main()
 {
-	int arr[] = { 1,2,2,4,9,9,1,9 };
+	int arr[] = { 1,3,2,4,7,9,1,9 };
 	position(arr,8);
 
 
