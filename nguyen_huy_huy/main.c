d7fb12aedaf7283426a4ca209775a168b22ea3af
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-20 10:08:54 +0700

    Bai 2: tinh trung binh cong cac so le trong mang

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -1,28 +1,39 @@
 ﻿#include <stdio.h>
 #include <math.h>
 
-//Chức năng: Tính tổng giá trị của mảng
+//Chức năng: Tính TBC cac phan tu la so le của mảng
 //Input: mảng, số lượng phần tử
 // (int* array, so_luong)
-//Output: Tổng của các phần tử trong mảng
-int sum_array(int* array, int num_elements)
+//Output: TBC của các phần tử lẻ trong mảng
+float average(int* array, int num_elements)
 {
-	int sum = 0;
+	
+	float sum_odd = 0;
+	float odd = 0;
+	float average_odd = 0;
+
 	for (int i = 0; i < num_elements; i++)
 	{
-		sum = sum + *(array+i);
+		if (*(array+i) % 2 != 0)
+		{
+			odd += *(array + i);
+			sum_odd++;
+		}
 	}
-	return sum;
+	average_odd = odd / sum_odd;
+	return average_odd;
 
 }
 
 
 int main()
 {
-	int tong = 0;
-	int arr[] = { 1,2,3 };
-	tong = sum_array(arr, 3);
-	printf("The sum of element of array is:%d", tong);
+	float TBC = 0;
+	int arr[] = { 1,2,3,4,7 };
+	TBC = average(arr, 5);
+
+	printf("TBC la: %f", TBC);
+
 }
 
 
