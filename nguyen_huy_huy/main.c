1889e14944ad93cf5426f602755f58f175db0685
huyspkt2003 <nguyenhuuhuylqd@gmail.com>
2025-03-20 16:31:17 +0700

    Bài 4: Xây dựng hàm. dùng để điếm số từ (word) trong chuỗi

--- nguyen_huy_huy/main.c
+++ nguyen_huy_huy/main.c
@@ -4,27 +4,28 @@
 //Chức năng: dùng để chuyễn chữ thường thành chư IN HOA
 //Input: chuỗi
 // (char* string)
-//Output: no
-void upper_case(char* string)
+//Output: so tu trong chuoi
+void sum_word(char* string)
 {
 	int sum_ele = 0;
-	int sum_space = 0;
+	int sum_word = 0;
 	while (string [sum_ele] != 0)
 	{
-		if (string[sum_ele] >= 97)
+		if (string[sum_ele] == 32 && string[sum_ele+1]!=32  )
 		{
-			string[sum_ele] -= 32;
+			sum_word++;
 		}
 		sum_ele++;
 
 	}
+	printf("%d", sum_word);
 }
 
 int main()
 {
 	int tong = 0;
-	char str[] = "Hello world hihi";
-	upper_case(str);
+	char str[] = " Hello world hihi";
+	sum_word(str);
 	
 
 }
